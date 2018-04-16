d3daa0a6a08e761da3c4359913e726fb13b23830
jjade seravla <jadealvares@hotmail.co.uk>
2018-04-16 11:03:46 +0100

    updated README.md and node modules

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# EmbeddedAudioFiles
+# Embedded Audio Files
+Plays audio files from SoundCloud using their API on a test page.
+
+## Setup:
+
+1 - ``` npm install ```  
+
+2 - ```npm start```
+Navigate to localhost:3000 to see the page
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const http = require('http');
 const fs = require('fs');
+const SC = require('soundcloud');
+
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -19,4 +21,14 @@ fs.readFile('index.html', (err, html) => {
   server.listen(port, hostname, () => {
     console.log('Server started on port ' +port);
   });
+});
+
+
+SC.initialize({
+  client_id: 'YOUR_CLIENT_ID',
+  redirect_uri: 'http://example.com/callback'
+});
+
+SC.get('/user/183/tracks').then(function(tracks){
+  alert('Latest track: ' + tracks[0].title);
 });
